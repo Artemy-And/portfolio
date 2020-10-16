1d0d50705b8aa6bced5cc21c3ccc767544e3f41e
Artemy-And <andruschak.artemy@gmail.com>
2020-10-16 12:33:57 +0300

    Updates

---
+++ precache-manifest.bffd313dd5cc1f4a0d6a1e17c17bc03f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918f5f89865a4db9c67f633fc4c2d5ab",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "537c34dc2f76566980a9",
+    "url": "/portfolio/static/css/main.60b51f89.chunk.css"
+  },
+  {
+    "revision": "50ce25d48da361807bf9",
+    "url": "/portfolio/static/js/2.eb5937ce.chunk.js"
+  },
+  {
+    "revision": "9b73840e8f87bae10f937ec158a37518",
+    "url": "/portfolio/static/js/2.eb5937ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537c34dc2f76566980a9",
+    "url": "/portfolio/static/js/main.d1d61db5.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  },
+  {
+    "revision": "a3f07c2470473c133bd2a102d0e54655",
+    "url": "/portfolio/static/media/Facebook.a3f07c24.jpg"
+  },
+  {
+    "revision": "62215dbfbd614c6ccaf11466d93714b5",
+    "url": "/portfolio/static/media/instagram.62215dbf.jpeg"
+  },
+  {
+    "revision": "1276b4e9a694f190a040e5d124a81d58",
+    "url": "/portfolio/static/media/logistics-socialnet.1276b4e9.jpg"
+  },
+  {
+    "revision": "f599c6c3bd71b6f0324d80bf9c885ecb",
+    "url": "/portfolio/static/media/photo.f599c6c3.jpeg"
+  },
+  {
+    "revision": "a7b4670d4204364107387f260b8b5fde",
+    "url": "/portfolio/static/media/todolist.a7b4670d.jpg"
+  }
+]);
